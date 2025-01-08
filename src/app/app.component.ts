fcec8904a67907ec49c4a7506d459b956a006677
Fredrick Denga <dengafred@gmail.com>
2025-01-08 11:02:16 -0800

    setup ngx-bootstrap and assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/core/navbar/navbar.component';
+import { HomeComponent } from './components/core/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'course-ui';
